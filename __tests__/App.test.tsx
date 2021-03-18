93e86df5cafd2a332b39a1284c172bbbea684b56
Joaquim <johnboxcodes@gmail.com>
2021-03-18 12:53:09 -0300

    update test

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -3,7 +3,6 @@ import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import App from '../src/App'
 
-
 test('renders a message', () => {
   const { container, getByText } = render(<App />)
   expect(getByText('hello, world!')).toBeInTheDocument()
