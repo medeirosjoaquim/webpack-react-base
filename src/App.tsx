cc0ada2b78ad2024b3ad8980190957970fce95d7
Joaquim <johnboxcodes@gmail.com>
2021-03-18 20:07:59 -0300

    adds prettier and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function App() {
-    return (
-        <div>
-            <h1>hello, world!</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>hello, world!</h1>
+    </div>
+  );
 }
